6c3a21b9151124be3ab2a1a7444358c0b2f2eb08
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-11-27 11:45:50 +0900

    accept multiple arguments

--- main.go
+++ main.go
@@ -13,18 +13,19 @@ func main() {
 
 	args := flag.Args()
 	if len(args) == 0 {
-		fmt.Println("no arguments\tusage:: unixsec unix-seconds")
+		fmt.Println("no arguments\tusage:: unixsec unix-seconds...")
 		os.Exit(1)
 	}
 
-	v := args[0]
-	unixSec, err := strconv.ParseInt(v, 10, 64)
-	if err != nil {
-		fmt.Printf("invalid value [%v]\n", v)
-		os.Exit(1)
-	}
+	for _, v := range args {
+		unixSec, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			fmt.Printf("invalid value [%v]\n", v)
+			os.Exit(1)
+		}
 
-	fmt.Printf("%v\t-->\t%v\n", unixSec, time.Unix(unixSec, 0))
+		fmt.Printf("%v\t-->\t%v\n", unixSec, time.Unix(unixSec, 0))
+	}
 
 	os.Exit(0)
 }
